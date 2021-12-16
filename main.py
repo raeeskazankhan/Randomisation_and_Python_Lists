f88af7ef4d8c566de5ffa4542a9a24edc96f00a0
raeeskazankhan <m215027016@gmail.com>
2021-12-16 21:59:04 +0200

    Added Banker Roulette Game Code

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ import random
 
 # HEADs OR TAILS
 
-random_dice = random.randint(0, 1)
+# random_dice = random.randint(0, 1)
+#
+# if random_dice == 0:
+#     print("Tails")
+# else:
+#     print("Heads")
 
-if random_dice == 0:
-    print("Tails")
-else:
-    print("Heads")
+# BANKER ROULETTE
+names_string = input("Give everybody's name, separated by a comma: ")
+names = names_string.split(", ")
+
+random_person = random.randint(0, len(names) - 1)
+person_to_pay = names[random_person]
+
+print(f"{person_to_pay} is going to pay for the meal today!")
