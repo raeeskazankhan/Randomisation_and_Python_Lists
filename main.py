e8fdff281a60ddee7703e3e4cb978fb129d72553
raeeskazankhan <m215027016@gmail.com>
2021-12-16 21:44:03 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# random_float = random.random() * 5
+# print(random_float)
+
+# HEADs OR TAILS
+
+random_dice = random.randint(0, 1)
+
+if random_dice == 0:
+    print("Tails")
+else:
+    print("Heads")
